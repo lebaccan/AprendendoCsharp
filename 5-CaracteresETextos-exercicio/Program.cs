545ce30afb983c3b8079431675d33fe96d89a5f6
Leandro Roberto <leandrobaccan76@gmail.com>
2021-07-05 22:34:40 -0300

    aula-05-07-21

---
+++ 5-CaracteresETextos-exercicio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos_exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char letra = 'a';
+            Console.WriteLine(letra);
+
+            char valor = (char)65;
+            Console.WriteLine(valor);
+
+            valor = (char)(valor + 1);
+            Console.WriteLine(valor);
+
+            string palavra = "Alura cursos online de Tecnologia " + 2020;
+            Console.WriteLine(palavra);
+
+
+            Console.WriteLine("Programa finalizado, tecle enter...");
+            Console.ReadLine();
+
+        }
+    }
+}
