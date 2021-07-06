ed70a3331a1a1554d258e1feca4beb8a579e6373
Leandro Roberto <leandrobaccan76@gmail.com>
2021-07-05 23:05:10 -0300

    aula atribuicoesvariaveis

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Executanto 6 - AtribuicoesDeVariaveis");
+
+            int idade = 44;
+            int idadeLeandro = idade;
+
+            idade = 45;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeLeandro);
+
+
+
+            Console.WriteLine("Press enter. . . ");
+            Console.ReadLine();
+        }
+    }
+}
