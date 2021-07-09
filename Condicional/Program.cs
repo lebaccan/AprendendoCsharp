4f236f8238af198e7c4631c6873aa8ff828f1d90
Leandro Roberto <leandrobaccan76@gmail.com>
2021-07-08 22:15:34 -0300

    condiconal

---
+++ Condicional/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("testando condicionais");
+
+            int idade = 20;
+            int quantidadePessoas = 3;
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Você tem mais de 18 anos");
+                Console.WriteLine("Seja Bem Vindo!");
+
+            }
+            else
+            {
+                if (quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("voce nao tem 18, mas pode entrar, pois está acompanhado");
+                }
+                else
+                {
+                    Console.WriteLine("Infelizmente você não pode entrar");
+                }
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
