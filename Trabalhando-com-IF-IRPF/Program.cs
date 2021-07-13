6b2d81cce583ae300f08333fe2bd43ec5990767c
Leandro Roberto <leandrobaccan76@gmail.com>
2021-07-12 23:47:34 -0300

    aula-12-07

---
+++ Trabalhando-com-IF-IRPF/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalhando_com_IF_IRPF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 3300.0;
+
+            if (salario <= 2600.0)
+                Console.WriteLine("A sua aliquota é de 15%");
+                Console.WriteLine("Você pode deduzir até R$ 350");
+
+            if (salario <= 3750.0)
+                Console.WriteLine("A sua aliquota é de 22,5%");
+                Console.WriteLine("Você pode deduzir até R$ 636");
+
+            Console.ReadLine();
+        }
+    }
+}
