47dbdc9fd57ea722961d610d49de880d6b03c136
Leandro Roberto <leandrobaccan76@gmail.com>
2021-07-26 23:38:28 -0300

    certificado-csharp-first

---
+++ Multiplos-3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplos_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            // outra maneira mais simples é:     for (int i = 3; i < 100; i += 3 )
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
